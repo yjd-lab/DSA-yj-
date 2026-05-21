e54bb3c0b4624650cf732334bf6ff6a5b50c08fd
yjd-lab <yjdj21993@gmail.com>
2026-05-21 22:12:30 +0530

    Time: 3 ms (21.30%) | Memory: 43.3 MB (12.45%) - LeetSync

---
+++ 1205-defanging-an-ip-address/defanging-an-ip-address.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String defangIPaddr(String address) {
+        String result = "";
+
+        for (int i = 0; i < address.length(); i++) {
+            if (address.charAt(i) == '.') {
+                result += "[.]";
+            } else {
+                result += address.charAt(i);
+            }
+        }
+
+        return result;
+    }
+}
+
+/* EASIEST SOLUTION:
+class Solution {
+    public String defangIPaddr(String address) {
+        return address.replace(".", "[.]");
+    }
+}*/
