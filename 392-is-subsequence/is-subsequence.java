bfe26aeda7f900ecdddd5b12e0702562b4b67363
yjd-lab <yjdj21993@gmail.com>
2026-05-23 15:41:14 +0530

    Time: 2 ms (70.55%) | Memory: 42.8 MB (44.13%) - LeetSync

---
+++ 392-is-subsequence/is-subsequence.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        if(s== null || s.equals("")){
+            return true;
+        }
+        int index = 0;
+        for (int i = 0 ; i<t.length();i++){
+            if(s.charAt(index)== t.charAt(i)){
+                index++;
+            }
+            if(index==s.length()){
+                return true;
+            }
+        }return false;
+    }
+}
