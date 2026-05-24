d5886eaa609a1d3cf7ded021c5103cae402d60ca
yjd-lab <yjdj21993@gmail.com>
2026-05-24 15:53:33 +0530

    Time: 14 ms (27.80%) | Memory: 46.8 MB (8.83%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,48 +1,17 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-         if(s.length() != t.length()) {
+        if(s.length()!= t.length()){
             return false;
         }
-        int[] count = new int[26];
-        for (char ch : s.toCharArray()){
-            count[ch-'a']++;
+        HashMap<Character,Integer> n = new HashMap<>();
+        for(char ch : s.toCharArray()){
+            n.put(ch,n.getOrDefault(ch,0)+1);
         }
-        for (char ch : t.toCharArray()){
-            count[ch-'a']--;
-            if( count[ch-'a']<0){
+        for(char ch : t.toCharArray()){
+            if(!n.containsKey(ch)||n.get(ch)==0){
                 return false;
             }
-        } return true;
+            n.put(ch,n.get(ch)-1);
+        }return true;
     }
 }
-/* 
-class Solution {
-    public boolean isAnagram(String s, String t) {
-
-        // Optional optimization
-        if(s.length() != t.length()) {
-            return false;
-        }
-
-        HashMap<Character, Integer> map = new HashMap<>();
-
-        // Count characters from s
-        for(char ch : s.toCharArray()) {
-            map.put(ch, map.getOrDefault(ch, 0) + 1);
-        }
-
-        // Remove characters using t
-        for(char ch : t.toCharArray()) {
-
-            if(!map.containsKey(ch) || map.get(ch) == 0) {
-                return false;
-            }
-
-            map.put(ch, map.get(ch) - 1);
-        }
-
-        return true;
-    }
-}
-
- */
