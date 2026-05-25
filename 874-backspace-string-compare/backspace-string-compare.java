ebffebcbc6a0ec5c92eb95def56b10aa668683d2
yjd-lab <yjdj21993@gmail.com>
2026-05-25 12:54:58 +0530

    Time: 1 ms (80.53%) | Memory: 42.8 MB (76.54%) - LeetSync

--- 874-backspace-string-compare/backspace-string-compare.java
+++ 874-backspace-string-compare/backspace-string-compare.java
@@ -1,16 +1,25 @@
 class Solution {
+
     public boolean backspaceCompare(String s, String t) {
+
         return build(s).equals(build(t));
     }
-    public String build(String str){
-        StringBuilder sb = new StringBuilder();
-        for (char ch : str.toCharArray() ){
-            if(ch != '#'){
-                sb.append(ch);
+
+    public String build(String str) {
+
+        StringBuilder string = new StringBuilder();
+
+        for(int i = 0; i < str.length(); i++) {
+
+            if(str.charAt(i) != '#') {
+                string.append(str.charAt(i));
             }
-            else if (sb.length()>0){
-                sb.deleteCharAt(sb.length()-1);
+
+            else if(string.length() > 0) {
+                string.deleteCharAt(string.length() - 1);
             }
-        }return sb.toString();
+        }
+
+        return string.toString();
     }
 }
