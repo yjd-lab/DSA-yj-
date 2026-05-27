7755ed128e335c0e25bf11a49fe1a1836035ab89
yjd-lab <yjdj21993@gmail.com>
2026-05-27 13:30:57 +0530

    Time: 69 ms (17.79%) | Memory: 49.1 MB (5.02%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
@@ -1,47 +1,28 @@
 class Solution {
-
     public List<Integer> findAnagrams(String s, String p) {
-
         List<Integer> res = new ArrayList<>();
-
         if(s.length() < p.length()) {
             return res;
         }
-
         HashMap<Character, Integer> pmap = new HashMap<>();
         HashMap<Character, Integer> smap = new HashMap<>();
-
-        for(int i = 0; i < p.length(); i++) {
-
-            pmap.put(p.charAt(i),
-            pmap.getOrDefault(p.charAt(i), 0) + 1);
-
-            smap.put(s.charAt(i),
-            smap.getOrDefault(s.charAt(i), 0) + 1);
-        }
-
-        if(smap.equals(pmap)) {
-            res.add(0);
-        }
-
-        for(int i = p.length(); i < s.length(); i++) {
-
-            // add new character
+        for (int i = 0 ; i< p.length(); i++){
+            pmap.put(p.charAt(i),pmap.getOrDefault(p.charAt(i), 0) + 1);
             smap.put(s.charAt(i),smap.getOrDefault(s.charAt(i), 0) + 1);
-
-            // remove old character
-            char leftChar = s.charAt(i - p.length());
-            smap.put(leftChar,smap.get(leftChar) - 1);
-
-            if(smap.get(leftChar) == 0) {
-                smap.remove(leftChar);
+        }
+        if(smap.equals(pmap)){
+             res.add(0);
+        }
+        for(int i = p.length(); i< s.length() ; i++){
+            smap.put(s.charAt(i),smap.getOrDefault(s.charAt(i), 0) + 1);
+            smap.put(s.charAt(i-p.length()),smap.getOrDefault(s.charAt(i-p.length()), 0) - 1);
+            if(smap.get(s.charAt(i-p.length()))== 0 ){
+                smap.remove(s.charAt(i-p.length()));
             }
-
-            if(smap.equals(pmap)) {
-                res.add(i - p.length() + 1);
+            if(smap.equals(pmap)){
+                 res.add(i-p.length()+1);
             }
         }
-
         return res;
     }
 }
