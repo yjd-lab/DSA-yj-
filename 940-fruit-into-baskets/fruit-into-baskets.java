7f469416911ff5032349beb7a0781bc2dc057f6c
yjd-lab <yjdj21993@gmail.com>
2026-05-28 14:42:39 +0530

    Time: 56 ms (28.08%) | Memory: 70.6 MB (40.29%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.java
+++ 940-fruit-into-baskets/fruit-into-baskets.java
@@ -1,34 +1,19 @@
 class Solution {
-
     public int totalFruit(int[] fruits) {
-
         int left = 0;
-        int res = 0;
-
-        HashMap<Integer, Integer> count = new HashMap<>();
-
-        for(int right = 0; right < fruits.length; right++) {
-
+        int max = 0;
+        HashMap<Integer,Integer> count = new HashMap<>();
+        for (int right = 0 ; right< fruits.length ; right++){
             count.put(fruits[right],
             count.getOrDefault(fruits[right], 0) + 1);
-
-            while(count.size() > 2) {
-
-                int leftFruit = fruits[left];
-
-                count.put(leftFruit,
-                count.get(leftFruit) - 1);
-
-                if(count.get(leftFruit) == 0) {
-                    count.remove(leftFruit);
+            while(count.size()>2){
+                count.put(fruits[left],
+                count.getOrDefault(fruits[left], 0) - 1);
+                if(count.get(fruits[left])==0){
+                    count.remove(fruits[left]);
                 }
-
                 left++;
-            }
-
-            res = Math.max(res, right - left + 1);
-        }
-
-        return res;
+            } max = Math.max(max, right - left + 1);
+        }return max;
     }
 }
