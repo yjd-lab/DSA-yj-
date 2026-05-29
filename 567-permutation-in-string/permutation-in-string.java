bf0a3d7dd367039e199b9d9ee3d3ab6b533a01c2
yjd-lab <yjdj21993@gmail.com>
2026-05-29 14:59:31 +0530

    Time: 35 ms (18.89%) | Memory: 47 MB (11.06%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -1,28 +1,50 @@
 class Solution {
+
     public boolean checkInclusion(String s1, String s2) {
-        if(s1.length() > s2.length()){
+
+        if(s1.length() > s2.length()) {
             return false;
         }
-        HashMap<Character, Integer> map1 = new HashMap<>();
-        HashMap<Character, Integer> map2 = new HashMap<>();
-        for (int i =0 ; i < s1.length();i++){
-            map1.put(s1.charAt(i),map1.getOrDefault(s1.charAt(i),0)+1);
-            map2.put(s2.charAt(i),map2.getOrDefault(s2.charAt(i),0)+1);
+
+        HashMap<Character, Integer> m1 = new HashMap<>();
+        HashMap<Character, Integer> m2 = new HashMap<>();
+
+        for(int i = 0; i < s1.length(); i++) {
+
+            m1.put(s1.charAt(i),
+            m1.getOrDefault(s1.charAt(i), 0) + 1);
+
+            m2.put(s2.charAt(i),
+            m2.getOrDefault(s2.charAt(i), 0) + 1);
         }
-        if(map2.equals(map1)){
+
+        if(m1.equals(m2)) {
             return true;
         }
-        for (int i =s1.length() ; i<s2.length( ); i++){
-            map2.put(s2.charAt(i),map2.getOrDefault(s2.charAt(i),0)+1);
-            map2.put(s2.charAt(i-s1.length()),map2.getOrDefault(s2.charAt(i-s1.length()),0)-1);
-            if(map2.get(s2.charAt(i-s1.length()))==0){
-                map2.remove(s2.charAt(i-s1.length()));
-            }
-            if(map2.equals(map1)){
-            return true;
+
+        int j = 0;
+
+        for(int i = s1.length(); i < s2.length(); i++) {
+
+            char leftChar = s2.charAt(j);
+
+            m2.put(leftChar,
+            m2.get(leftChar) - 1);
+
+            if(m2.get(leftChar) == 0) {
+                m2.remove(leftChar);
             }
 
+            m2.put(s2.charAt(i),
+            m2.getOrDefault(s2.charAt(i), 0) + 1);
+
+            j++;
+
+            if(m1.equals(m2)) {
+                return true;
+            }
         }
+
         return false;
     }
 }
