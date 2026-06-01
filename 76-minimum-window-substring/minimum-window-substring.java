91c0645e66b8eb2d898ba2fe61260554c03bcbb1
yjd-lab <yjdj21993@gmail.com>
2026-06-01 14:18:56 +0530

    Time: 17 ms (52.51%) | Memory: 47.8 MB (19.63%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -4,31 +4,31 @@ class Solution {
             return "";
         }
         int j = 0;
-        int required = t.length();
-        HashMap<Character, Integer> map = new HashMap<>();
-        int start =0;
+        int start = 0;
         int min = Integer.MAX_VALUE;
-        for(int i=0 ; i< t.length(); i++){
+        int req= t.length();
+        HashMap<Character , Integer> map = new HashMap<>();
+        for(int i = 0 ; i< t.length() ; i++){
             map.put(t.charAt(i),map.getOrDefault(t.charAt(i),0)+1);
         }
-        for(int i = 0; i< s.length(); i++){
+        for(int i = 0 ; i< s.length() ; i++){
             if(map.containsKey(s.charAt(i))){
-                if(map.get(s.charAt(i)) > 0) {
-                    required--;
+                if(map.get(s.charAt(i)) > 0) { // i forget this lone always
+                    req--;
                 }
                 map.put(s.charAt(i),map.get(s.charAt(i))-1);
             }
-            while(required==0){
-                if(i-j+1 < min){
+            while(req==0){
+                if(i-j+1<min){
                     min = i-j+1;
-                    start = j ;
+                    start =j ;
                 }
                 if(map.containsKey(s.charAt(j))){
-                    
                     map.put(s.charAt(j),map.get(s.charAt(j))+1);
                     if(map.get(s.charAt(j))>0){
-                        required++;
+                        req++;
                     }
+
                 }
                 j++;
             }
