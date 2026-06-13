42cbad2e304936dc2d5fd62ff16cc39083744801
yjd-lab <yjdj21993@gmail.com>
2026-06-13 23:28:09 +0530

    Time: 2 ms (96.31%) | Memory: 47.9 MB (98.29%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.java
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.java
@@ -24,3 +24,36 @@ class Solution {
         return new int[]{};
     }
 }
+/*
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+
+        for(int i = 0; i < numbers.length; i++){
+
+            int need = target - numbers[i];
+
+            int left = i + 1;
+            int right = numbers.length - 1;
+
+            while(left <= right){
+
+                int mid = left + (right - left) / 2;
+
+                if(numbers[mid] == need){
+                    return new int[]{i + 1, mid + 1};
+                }
+
+                else if(numbers[mid] < need){
+                    left = mid + 1;
+                }
+
+                else{
+                    right = mid - 1;
+                }
+            }
+        }
+
+        return new int[]{};
+    }
+}
+*/
