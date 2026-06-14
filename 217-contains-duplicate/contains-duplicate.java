59bab382b2eb1182622ee3ff461d8b033e2b7af3
yjd-lab <yjdj21993@gmail.com>
2026-06-14 11:35:56 +0530

    Time: 16 ms (41.94%) | Memory: 98 MB (6.34%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -10,3 +10,17 @@ class Solution {
         return false;
     }
 }
+/*
+correct one :
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> seen = new HashSet<>();
+        for (int num : nums) {
+            if (seen.contains(num))
+                return true;
+            seen.add(num);
+        }
+        return false;
+    }
+}
+*/
