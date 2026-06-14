b08a175703f126244febecdab11d4b3d75cd12f4
yjd-lab <yjdj21993@gmail.com>
2026-06-14 12:08:38 +0530

    Time: 18 ms (13.98%) | Memory: 46.4 MB (32.60%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,17 +1,38 @@
-class Solution {
-    public boolean isAnagram(String s, String t) {
-        if(s.length()!= t.length()){
+import java.util.HashMap;
+
+class Solution { 
+    public boolean isAnagram(String s, String t) { 
+        // If lengths don't match, they cannot be anagrams
+        if (s.length() != t.length()) {
             return false;
         }
-        HashMap<Character,Integer> n = new HashMap<>();
-        for(char ch : s.toCharArray()){
-            n.put(ch,n.getOrDefault(ch,0)+1);
-        }
-        for(char ch : t.toCharArray()){
-            if(!n.containsKey(ch)||n.get(ch)==0){
+
+        HashMap<Character, Integer> map = new HashMap<>(); 
+        
+        // Count frequencies of characters in string s
+        for (int i = 0; i < s.length(); i++) { 
+            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1); 
+        } 
+        
+        // Decrement frequencies using string t
+        for (int i = 0; i < t.length(); i++) { 
+            char c = t.charAt(i);
+            
+            // If character doesn't exist in the map, it's not an anagram
+            if (!map.containsKey(c)) {
                 return false;
             }
-            n.put(ch,n.get(ch)-1);
-        }return true;
-    }
+            
+            // Corrected put syntax: key followed by the decremented value
+            map.put(c, map.get(c) - 1); 
+            
+            // Corrected remove method
+            if (map.get(c) == 0) { 
+                map.remove(c); 
+            } 
+        } 
+        
+        // If the map is empty, all character counts matched perfectly
+        return map.isEmpty(); 
+    } 
 }
