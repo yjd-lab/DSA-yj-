6dddb536d19520b1f29d0e872e4f322aa16f4424
yjd-lab <yjdj21993@gmail.com>
2026-06-15 16:51:55 +0530

    Time: 5 ms (35.38%) | Memory: 69.7 MB (24.40%) - LeetSync

--- 643-maximum-average-subarray-i/maximum-average-subarray-i.java
+++ 643-maximum-average-subarray-i/maximum-average-subarray-i.java
@@ -16,3 +16,22 @@ class Solution {
         return max;
     }
 }
+/*more optimized way :
+class Solution {
+    public double findMaxAverage(int[] nums, int k) {
+        double sum = 0;
+        for (int i = 0; i < k; i++) {
+            sum += nums[i];
+        }
+        
+        double maxSum = sum;
+        for (int i = k; i < nums.length; i++) {
+            sum += nums[i] - nums[i - k]; // Avoids needing a separate 'j' pointer
+            maxSum = Math.max(maxSum, sum);
+        }
+        
+        return maxSum / k;
+    }
+}
+
+*/
