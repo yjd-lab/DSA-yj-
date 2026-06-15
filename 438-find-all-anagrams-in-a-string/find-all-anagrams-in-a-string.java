316b06d94d0c95ed241414d5af3554d9025760fb
yjd-lab <yjdj21993@gmail.com>
2026-06-15 19:39:29 +0530

    Time: 65 ms (19.86%) | Memory: 48.2 MB (6.05%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
@@ -1,31 +1,30 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
-        
+        List<Integer> list = new ArrayList<>();
+        if(p.length() > s.length()) {
+            return list;
+        }
         int j = 0;
-        List<Integer> l = new ArrayList<>();
-        HashMap<Character ,Integer> s1 = new HashMap<>();
-        HashMap<Character ,Integer> p1 = new HashMap<>();
-        if(s.length()<p.length()){
-            return l;
+        HashMap<Character,Integer> m1 = new HashMap<>();
+        HashMap<Character,Integer> m2 = new HashMap<>();
+        for(int i = 0; i <p.length();i++){
+            m1.put(p.charAt(i),m1.getOrDefault(p.charAt(i),0)+1);
+            m2.put(s.charAt(i),m2.getOrDefault(s.charAt(i),0)+1);
         }
-        for (int i = 0  ; i< p.length() ; i++){
-            s1.put(s.charAt(i),s1.getOrDefault(s.charAt(i),0)+1);
-            p1.put(p.charAt(i),p1.getOrDefault(p.charAt(i),0)+1);
+        if(m1.equals(m2)){
+            list.add(0);
         }
-        if(s1.equals(p1)){
-            l.add(0);
-        }
-        for(int i = p.length(); i< s.length() ;i++){
-            s1.put(s.charAt(i),s1.getOrDefault(s.charAt(i),0)+1);
-            s1.put(s.charAt(j),s1.getOrDefault(s.charAt(j),0)-1);
-            if(s1.get(s.charAt(j))==0){
-                s1.remove(s.charAt(j));
+        for(int i =p.length();i<s.length();i++){
+            m2.put(s.charAt(j),m2.get(s.charAt(j))-1);
+            if(m2.get(s.charAt(j))==0){
+                m2.remove(s.charAt(j));
             }
             j++;
-            if(s1.equals(p1)){
-                l.add(j);
+            m2.put(s.charAt(i),m2.getOrDefault(s.charAt(i),0)+1);
+            if(m1.equals(m2)){
+                list.add(j);
             }
         }
-        return  l ;
+        return list;
     }
 }
