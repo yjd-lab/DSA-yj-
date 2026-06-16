bfae6c03ff1199dcbce92536e530d057924d25c7
yjd-lab <yjdj21993@gmail.com>
2026-06-16 12:42:36 +0530

    Time: 3 ms (61.79%) | Memory: 43.5 MB (61.18%) - LeetSync

--- 1666-make-the-string-great/make-the-string-great.java
+++ 1666-make-the-string-great/make-the-string-great.java
@@ -1,31 +1,22 @@
+import java.util.Stack;
+
 class Solution {
-
     public String makeGood(String s) {
-
         Stack<Character> stk = new Stack<>();
-
-        for(int i = 0; i < s.length(); i++) {
-
-            char ch = s.charAt(i);
-
-            if(!stk.isEmpty() &&
-               Math.abs(stk.peek() - ch) == 32) {
-
+        for (char i : s.toCharArray()) {
+            // Use an if statement instead of a while loop
+            if (!stk.isEmpty() && Math.abs(i - stk.peek()) == 32) {
                 stk.pop();
-            }
-
-            else {
-
-                stk.push(ch);
+            } else {
+                stk.push(i);
             }
         }
-
+        
+        // Build the string from the remaining characters in the stack
         StringBuilder str = new StringBuilder();
-
-        for(char c : stk) {
-            str.append(c);
+        for (char ch : stk) {
+            str.append(ch);
         }
-
         return str.toString();
     }
 }
