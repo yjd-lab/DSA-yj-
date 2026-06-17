1c9ccd44e00cfde882f7238759d2ac5563a4061c
yjd-lab <yjdj21993@gmail.com>
2026-06-17 15:31:22 +0530

    Time: 47 ms (46.86%) | Memory: 79 MB (65.62%) - LeetSync

--- 1675-magnetic-force-between-two-balls/magnetic-force-between-two-balls.java
+++ 1675-magnetic-force-between-two-balls/magnetic-force-between-two-balls.java
@@ -1,47 +1,36 @@
+import java.util.Arrays;
+
 class Solution {
-
     public int maxDistance(int[] position, int m) {
-
         Arrays.sort(position);
-
         int left = 1;
         int right = position[position.length - 1] - position[0];
-
-        int res = 0;
-
-        while(left <= right){
-
+        int res = -1;
+        
+        while (left <= right) {
             int mid = left + (right - left) / 2;
-
-            if(canPlace(position, m, mid)){
-
+            if (canPlace(position, m, mid)) {
                 res = mid;
-                left = mid + 1;
-            }
-
-            else{
-
-                right = mid - 1;
+                left = mid + 1; // Try to look for a larger valid minimum distance
+            } else {
+                right = mid - 1; // Distance is too big, try smaller
             }
         }
-
         return res;
     }
-
-    public boolean canPlace(int[] position, int m, int dist){
-
-        int balls = 1;
+    
+    public boolean canPlace(int[] position, int m, int mid) {
+        int balls = 1; // First ball is always placed at position[0]
         int last = position[0];
-
-        for(int i = 1; i < position.length; i++){
-
-            if(position[i] - last >= dist){
-
+        
+        // FIXED: Start loop at index 1 since the first ball is already placed
+        for (int j = 1; j < position.length; j++) {
+            if (position[j] - last >= mid) {
                 balls++;
-                last = position[i];
+                last = position[j];
             }
         }
-
-        return balls >= m;
+        // FIXED: Returns true if we placed AT LEAST 'm' balls successfully
+        return balls >= m; 
     }
 }
