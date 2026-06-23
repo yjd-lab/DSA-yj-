96321bcb7b162ca0772c096da0f2ee276b6ffac5
yjd-lab <yjdj21993@gmail.com>
2026-06-23 21:02:45 +0530

    Time: 21 ms (92.95%) | Memory: 60.5 MB (5.85%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.java
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.java
@@ -1,10 +1,10 @@
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
         PriorityQueue<int[]> pq = new PriorityQueue<>(
-            (a, b) ->
-                (b[0]*b[0] + b[1]*b[1])
-                -
-                (a[0]*a[0] + a[1]*a[1])
+            (a,b) -> {
+                int dist1 = a[0] * a[0] + a[1] * a[1];
+                int dist2 = b[0] * b[0] + b[1] * b[1];
+                return dist2-dist1;}
         );
         for (int i[] : points){
             pq.add(i);
