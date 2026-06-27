6cf1c556d4b8d1c140ea3f2e9d3cc3e07c0265e7
yjd-lab <yjdj21993@gmail.com>
2026-06-27 09:52:12 +0530

    Time: 1 ms (96.43%) | Memory: 42.3 MB (93.71%) - LeetSync

--- 231-power-of-two/power-of-two.java
+++ 231-power-of-two/power-of-two.java
@@ -1,5 +1,4 @@
 class Solution { 
-    // This helper method now returns 1 if it's a power of two, and 0 if it's not
     public int power(int n) { 
         if (n <= 0) {
             return 0;
@@ -12,7 +11,6 @@ class Solution {
         }
         return power(n / 2); 
     } 
-
     public boolean isPowerOfTwo(int n) { 
         int res = power(n); 
         if (res == 1) { 
