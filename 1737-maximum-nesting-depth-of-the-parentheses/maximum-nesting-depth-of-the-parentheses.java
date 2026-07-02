80d10fde9f300da4e1e8551906fb521a60854793
yjd-lab <yjdj21993@gmail.com>
2026-07-02 11:10:39 +0530

    Time: 1 ms (24.21%) | Memory: 42.9 MB (52.75%) - LeetSync

--- 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.java
+++ 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.java
@@ -18,3 +18,21 @@ class Solution {
         return res;
     }
 }
+/*
+class Solution {
+    public int maxDepth(String s) {
+        int max=0;
+        int cnt=0;
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='('){
+                cnt++;
+            }
+            else if(s.charAt(i)==')'){
+                cnt--;
+            }
+            max=Math.max(max,cnt);
+        }
+        return max;
+    }
+}
+*/
